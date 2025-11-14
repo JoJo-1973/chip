49e96def629fd32e8e683173699d3007a0b555a3
JoJo-1973 <123166933+JoJo-1973@users.noreply.github.com>
2025-11-14 11:22:02 +0100

    Aggiunta mappa colori del VIC

--- vic.asm
+++ vic.asm
@@ -18,3 +18,23 @@ VICCRC            = VIC+$0C     ; Frequenza oscillatore #3 (soprano)
 VICCRD            = VIC+$0D     ; Frequenza oscillatore #4 (rumore)
 VICCRE            = VIC+$0E     ; Colore ausiliario / Volume
 VICCRF            = VIC+$0F     ; Colore sfondo / bordo / modalità inversa
+
+; Codici colore del chip VIC
+VIC_BLACK         = 0           ; Codice VIC del colore nero
+VIC_WHITE         = 1           ; Codice VIC del colore bianco
+VIC_RED           = 2           ; Codice VIC del colore rosso
+VIC_CYAN          = 3           ; Codice VIC del colore ciano
+VIC_PURPLE        = 4           ; Codice VIC del colore viola
+VIC_GREEN         = 5           ; Codice VIC del colore verde
+VIC_BLUE          = 6           ; Codice VIC del colore blu
+VIC_YELLOW        = 7           ; Codice VIC del colore giallo
+VIC_ORANGE        = 8           ; Codice VIC del colore arancione
+VIC_LIGHT_ORANGE  = 9           ; Codice VIC del colore arancione chiaro
+VIC_PINK          = 10          ; Codice VIC del colore rosa
+VIC_LIGHT_CYAN    = 11          ; Codice VIC del colore ciano chiaro
+VIC_LIGHT_PURPLE  = 12          ; Codice VIC del colore viola chiaro
+VIC_LIGHT_GREEN   = 13          ; Codice VIC del colore verde chiaro
+VIC_LIGHT_BLUE    = 14          ; Codice VIC del colore azzurro
+VIC_LIGHT_YELLOW  = 15          ; Codice VIC del colore giallo chiaro
+
+VIC_TRANSPARENT   = $FF         ; Valore fittizio usato per indicare che il registro colore non va alterato
